8e5b12866dcae15ae83b649945a2f33363f6a9a4
Janine Rugayan <janinelr@stud.ntnu.no>
2020-12-03 22:12:53 +0100

    bash script edited

--- egs/experiment.bash
+++ egs/experiment.bash
@@ -1,15 +1,18 @@
 cd ../scripts/
 
 # for shuffled
+echo "START shuffled"
 python main.py --data_name=recog_results_14-Oct_SHUFFLED_run3
 echo "shuffled done"
 
 # for not shuffled
+echo "START not shuffled"
 python main.py --data_name=recog_results_14-Oct_notshuffled-run1
 python main.py --data_name=recog_results_14-Oct_notshuffled-run3
 echo "not shuffled done"
 
 # for random segmentation
+echo "START random segmentation"
 python main.py --data_name=recog_results_randomseg-run3_dict
 python main.py --data_name=recog_results_randomseg-run1_dict
 echo "random segmentation done"
