6045d59bd81657c93d355e89e776bfb920642311
Janine Rugayan <janinelr@stud.ntnu.no>
2020-12-04 16:30:31 +0100

    try again re-run of those stuck at random seeds

--- egs/experiment.bash
+++ egs/experiment.bash
@@ -1,9 +1,15 @@
 cd ../scripts/
 
 # for shuffled
-# echo "START shuffled"
-# python main.py --data_name=recog_results_14-Oct_SHUFFLED_run3
-# echo "shuffled done"
+echo "START shuffled"
+python main.py --data_name=recog_results_14-Oct_SHUFFLED_run1
+python main.py --data_name=recog_results_14-Oct_SHUFFLED_run3
+echo "shuffled done"
+
+echo "========= START re-run of 03-Dec dictionary ========="
+# python main.py --data_name=recog_results_03-Dec_notshuffled
+python main.py --data_name=recog_results_03-Dec_SHUFFLED
+echo "========= re-run done ========="
 
 # # for not shuffled
 # echo "START not shuffled"
@@ -11,21 +17,16 @@ cd ../scripts/
 # python main.py --data_name=recog_results_14-Oct_notshuffled-run3
 # echo "not shuffled done"
 #
-# # for random segmentation
-# echo "START random segmentation"
+# for random segmentation
+echo "START random segmentation"
 # python main.py --data_name=recog_results_randomseg-run3_dict
-# python main.py --data_name=recog_results_randomseg-run1_dict
-# echo "random segmentation done"
+python main.py --data_name=recog_results_randomseg-run1_dict
+echo "random segmentation done"
 
 # rerun this batch:
-echo "========= START re-run of not-shuffled run2 ========="
-python main.py --data_name=recog_results_14-Oct_notshuffled-run2
-echo "========= START re-run of shuffled run2 ========="
-python main.py --data_name=recog_results_14-Oct_SHUFFLED_run2
-echo "========= START re-run of randomseg run2 ========="
-python main.py --data_name=recog_results_randomseg-run2_dict
-
-echo "========= START re-run of 03-Dec dictionary ========="
-python main.py --data_name=recog_results_03-Dec_notshuffled
-python main.py --data_name=recog_results_03-Dec_SHUFFLED
-echo "========= re-run done ========="
+# echo "========= START re-run of not-shuffled run2 ========="
+# python main.py --data_name=recog_results_14-Oct_notshuffled-run2
+# echo "========= START re-run of shuffled run2 ========="
+# python main.py --data_name=recog_results_14-Oct_SHUFFLED_run2
+# echo "========= START re-run of randomseg run2 ========="
+# python main.py --data_name=recog_results_randomseg-run2_dict
