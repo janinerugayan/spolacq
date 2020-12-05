bfd61166d027b89310a36d4786a1d33b66732ee8
Janine Rugayan <janinelr@stud.ntnu.no>
2020-12-05 19:24:00 +0100

    changed to 50 random seeds, new dict files

--- egs/experiment.bash
+++ egs/experiment.bash
@@ -6,8 +6,8 @@ python main.py --data_name=recog_results_05-Dec_SHUFFLED_run2
 python main.py --data_name=recog_results_05-Dec_SHUFFLED_run3
 echo "========= RL - SHUFFLED - done ========="
 
-# echo "========= START RL for 05-Dec dictionary ========="
-# python main.py --data_name=recog_results_05-Dec_notshuffled_run1
-# python main.py --data_name=recog_results_05-Dec_notshuffled_run2
-# python main.py --data_name=recog_results_05-Dec_notshuffled_run3
-# echo "========= RL - not shuffled - done ========="
+echo "========= START RL for 05-Dec dictionary ========="
+python main.py --data_name=recog_results_05-Dec_notshuffled_run1
+python main.py --data_name=recog_results_05-Dec_notshuffled_run2
+python main.py --data_name=recog_results_05-Dec_notshuffled_run3
+echo "========= RL - not shuffled - done ========="
