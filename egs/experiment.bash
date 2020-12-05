2770c761b132f69a2cab6e79e20b7c8a385f0c20
Janine Rugayan <janinelr@stud.ntnu.no>
2020-12-05 20:06:32 +0100

    for loop in bash script

--- egs/experiment.bash
+++ egs/experiment.bash
@@ -1,13 +1,18 @@
 cd ../scripts/
 
-echo "========= START RL for 05-Dec dictionary ========="
-python main.py --data_name=recog_results_05-Dec_SHUFFLED_run1
-python main.py --data_name=recog_results_05-Dec_SHUFFLED_run2
-python main.py --data_name=recog_results_05-Dec_SHUFFLED_run3
-echo "========= RL - SHUFFLED - done ========="
+for i in {1..20}
+do
+  echo "========= START RL for 05-Dec dictionary ========="
+  python main.py --data_name=recog_results_05-Dec_SHUFFLED_run1
+  python main.py --data_name=recog_results_05-Dec_SHUFFLED_run2
+  python main.py --data_name=recog_results_05-Dec_SHUFFLED_run3
+  echo "========= RL - SHUFFLED - done ========="
 
-echo "========= START RL for 05-Dec dictionary ========="
-python main.py --data_name=recog_results_05-Dec_notshuffled_run1
-python main.py --data_name=recog_results_05-Dec_notshuffled_run2
-python main.py --data_name=recog_results_05-Dec_notshuffled_run3
-echo "========= RL - not shuffled - done ========="
+  echo "========= START RL for 05-Dec dictionary ========="
+  python main.py --data_name=recog_results_05-Dec_notshuffled_run1
+  python main.py --data_name=recog_results_05-Dec_notshuffled_run2
+  python main.py --data_name=recog_results_05-Dec_notshuffled_run3
+  echo "========= RL - not shuffled - done ========="
+done
+
+echo "ALL DONE"
