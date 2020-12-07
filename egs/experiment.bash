2007e1a14163755b4877f2da28b06c5771197fad
Janine Rugayan <janinelr@stud.ntnu.no>
2020-12-07 14:21:34 +0100

    experiment for randomseg not shuffled dictionary

--- egs/experiment.bash
+++ egs/experiment.bash
@@ -17,14 +17,8 @@ cd ../scripts/
 #
 # echo "ALL DONE"
 
-echo "========= START RL for 05-Dec dictionary ========="
-python main.py --data_name=recog_results_05-Dec_SHUFFLED_run1
-python main.py --data_name=recog_results_05-Dec_SHUFFLED_run2
-python main.py --data_name=recog_results_05-Dec_SHUFFLED_run3
-echo "========= RL - SHUFFLED - done ========="
-
-echo "========= START RL for 05-Dec dictionary ========="
-python main.py --data_name=recog_results_05-Dec_notshuffled_run1
-python main.py --data_name=recog_results_05-Dec_notshuffled_run2
-python main.py --data_name=recog_results_05-Dec_notshuffled_run3
-echo "========= RL - not shuffled - done ========="
+echo "========= START RL for 07-Dec dictionary ========="
+python main.py --data_name=recog_results_07-Dec_notshuffled_randomseg_run1
+python main.py --data_name=recog_results_07-Dec_notshuffled_randomseg_run2
+python main.py --data_name=recog_results_07-Dec_notshuffled_randomseg_run3
+echo "========= RL - Not shuffled - Randomseg - done ========="
