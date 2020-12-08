edc47aca83fa790991e2ecfa6154f9de6584a17f
Janine Rugayan <janinelr@stud.ntnu.no>
2020-12-08 09:35:26 +0100

    experiment rerun 05-Dec dict for shuffled and not shuffled run 1

--- egs/experiment.bash
+++ egs/experiment.bash
@@ -1,24 +1,22 @@
 cd ../scripts/
 
-# for i in {1..20}
-# do
-#   echo "========= START RL for 05-Dec dictionary ========="
-#   python main.py --data_name=recog_results_05-Dec_SHUFFLED_run1
-#   python main.py --data_name=recog_results_05-Dec_SHUFFLED_run2
-#   python main.py --data_name=recog_results_05-Dec_SHUFFLED_run3
-#   echo "========= RL - SHUFFLED - done ========="
-#
-#   echo "========= START RL for 05-Dec dictionary ========="
-#   python main.py --data_name=recog_results_05-Dec_notshuffled_run1
-#   python main.py --data_name=recog_results_05-Dec_notshuffled_run2
-#   python main.py --data_name=recog_results_05-Dec_notshuffled_run3
-#   echo "========= RL - not shuffled - done ========="
-# done
+echo "========= START RL for 05-Dec dictionary ========="
+python main.py --data_name=recog_results_05-Dec_SHUFFLED_run1
+# python main.py --data_name=recog_results_05-Dec_SHUFFLED_run2
+# python main.py --data_name=recog_results_05-Dec_SHUFFLED_run3
+echo "========= RL - SHUFFLED - done ========="
+
+echo "========= START RL for 05-Dec dictionary ========="
+python main.py --data_name=recog_results_05-Dec_notshuffled_run1
+# python main.py --data_name=recog_results_05-Dec_notshuffled_run2
+# python main.py --data_name=recog_results_05-Dec_notshuffled_run3
+echo "========= RL - not shuffled - done ========="
+
 #
 # echo "ALL DONE"
 
-echo "========= START RL for 07-Dec dictionary ========="
-python main.py --data_name=recog_results_07-Dec_notshuffled_randomseg_run1
-python main.py --data_name=recog_results_07-Dec_notshuffled_randomseg_run2
-python main.py --data_name=recog_results_07-Dec_notshuffled_randomseg_run3
-echo "========= RL - Not shuffled - Randomseg - done ========="
+# echo "========= START RL for 07-Dec dictionary ========="
+# python main.py --data_name=recog_results_07-Dec_notshuffled_randomseg_run1
+# python main.py --data_name=recog_results_07-Dec_notshuffled_randomseg_run2
+# python main.py --data_name=recog_results_07-Dec_notshuffled_randomseg_run3
+# echo "========= RL - Not shuffled - Randomseg - done ========="
